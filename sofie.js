86561d9084f0afc322519f6f5e703ebeffb8234e
Kitinon Wangpattanamongkol <kitinon@hiqfood.com>
2019-09-09 07:36:11 +0700

    Use htmlparser.

--- sofie.js
+++ sofie.js
@@ -5,6 +5,14 @@ const fillTemplate = function(templateString, templateVars){
 export default class {
   constructor(template) {
     this.template = template
+
+    const handler = new Tautologistics.NodeHtmlParser.DefaultHandler((error, dom) => {
+      if (error) alert(error)
+      else console.log(JSON.stringify(dom, null, 2))
+    })
+    const parser = new Tautologistics.NodeHtmlParser.Parser(handler)
+    parser.parseComplete(template)
+    //alert(JSON.stringify(handler.dom, null, 2))
   }
 
   set(data) {
